409a46baf84ace335ec07870cfd2b76383b33226
bbedevian <brettbedevian@gmail.com>
2020-05-06 21:38:01 -0400

    added more games + update users score

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,40 +4,22 @@ require 'json'
 Word.destroy_all
 GameWord.destroy_all
 Game.destroy_all
+GameUser.destroy_all
+User.destroy_all
 
-Game.create(letters: "paeglmx")
+User.create(name: "brett")
+User.create(name: "ryan")
 
-array = ["agape",
-    "ample",
-    "apex",
-    "appall",
-    "appeal",
-    "appellee",
-    "apple",
-    "epee",
-    "example",
-    "exempla",
-    "expel",
-    "expellee",
-    "gape",
-    "lamp",
-    "lapel",
-    "leap",
-    "maple",
-    "megaplex",
-    "paella",
-    "page",
-    "pale",
-    "pall",
-    "palm",
-    "palp",
-    "pampa",
-    "papa",
-    "papal",
-    "peal",
-    "peel",
-    "peep",
-    "plea"]
+Game.create(letters: "paeglmx") #g1
+Game.create(letters: "uatrhfg") #g2
+Game.create(letters: "tarnlfg") #g3
+Game.create(letters: "lutofdb") #g4
+
+
+g1array = ["agape", "ample", "apex","appall", "appeal", "appellee", "apple", "epee", "example","exempla", "expel", "expellee", "gape", "lamp", "lapel", "leap", "maple", "megaplex", "paella", "page", "pale","pall","palm", "palp","pampa", "papa","papal", "peal", "peel", "peep", "plea"]
+g2array = ["aught","augur","aura","fraught","frug","fugu","gruff","guar", "guff", "guru", "huff", "hurrah", "hurt", "ruff", "taught", "taut",  "thug",  "truth", "tuft","turf","tutu"]
+g3array = ["altar","arrant", "attar", "fart", "fatal", "flagrant", "flat", "fragrant", "frat", "gallant", "gnat", "graft", "grant", "natal", "natant", "raft", "ragtag", "rant", "ratatat", "rattan", "tall", "tang", "tantara", "tantra","tart","tartan","tartar"]
+g4array = ["blob", "blood", "blot", "blotto", "bluff", "bold", "boll", "bolo", "bolt", "bulb", "bull", "doll", "dolt", "doubtful", "dull", "flood", "flout", "flub", "fluff", "fold", "foldout", "fool", "foul", "full", "lobo", "loft", "loll", "loot", "lotto", "loud", "lout", "lull", "lulu", "told", "toll", "tool", "tubful"]
 
     def calculate_points(input)
         if input.length == 4
@@ -45,25 +27,36 @@ array = ["agape",
         else 
             return input.length
         end
-      end # end of calc points 
+    end 
 
-
-   
-
-
-array.each do |word|
-    mykey = ENV["API_KEY"] 
-    url = RestClient.get("https://www.dictionaryapi.com/api/v3/references/collegiate/json/#{word}?key=#{mykey}")
-    word_parse = JSON.parse(url)
+g1array.each do |word|
     points = calculate_points(word)
     Word.create(spelling_word: word, point_value: points)
-
     wordID = Word.find_by(spelling_word: word)
     gameID = Game.find_by(letters: "paeglmx")
-    x = GameWord.create(word_id: wordID.id, game_id: gameID.id)
+    GameWord.create(word_id: wordID.id, game_id: gameID.id)
+end
 
-        if x.id 
-            puts "created #{word}"
-        end 
-    
+g2array.each do |word|
+    points = calculate_points(word)
+    Word.create(spelling_word: word, point_value: points)
+    wordID = Word.find_by(spelling_word: word)
+    gameID = Game.find_by(letters: "uatrhfg")
+    GameWord.create(word_id: wordID.id, game_id: gameID.id)
+end
+
+g3array.each do |word|
+    points = calculate_points(word)
+    Word.create(spelling_word: word, point_value: points)
+    wordID = Word.find_by(spelling_word: word)
+    gameID = Game.find_by(letters: "tarnlfg")
+    GameWord.create(word_id: wordID.id, game_id: gameID.id)
+end
+
+g4array.each do |word|
+    points = calculate_points(word)
+    Word.create(spelling_word: word, point_value: points)
+    wordID = Word.find_by(spelling_word: word)
+    gameID = Game.find_by(letters: "lutofdb")
+    GameWord.create(word_id: wordID.id, game_id: gameID.id)
 end
